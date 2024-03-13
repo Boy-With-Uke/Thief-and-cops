8c44eb32e0c89e53ccffe3427a368aac8056bd3d
BoyWithUke <117911858+Boy-With-Uke@users.noreply.github.com>
2024-03-13 21:04:11 +0300

    adding button

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,37 +2,34 @@ import { useState } from "react";
 import bgImage from "../assets/images/background.png";
 
 export default function Main() {
-  //NORTH
-  const [northNorth, setNorthNorth] = useState("");
-  const [northSouth, setNorthSouth] = useState("");
-  const [northEast, setNorthEast] = useState("");
-  const [northWest, setNorthWest] = useState("");
+  const [buttonTexts, setButtonTexts] = useState([
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+  ]);
 
-  function handleNorthNorthClick() {
-    setNorthNorth("X");
-    setNorthEast("");
-    setNorthSouth("");
-    setNorthWest("");
-  }
-
-  function handleNorthSouthClick() {
-    setNorthNorth("");
-    setNorthEast("");
-    setNorthSouth("X");
-    setNorthWest("");
-  }
-
-  function handleNorthEastClick() {
-    setNorthNorth("");
-    setNorthEast("X");
-    setNorthSouth("");
-    setNorthWest("");
-  }
-  function handleNorthWestClick() {
-    setNorthNorth("");
-    setNorthEast("");
-    setNorthSouth("");
-    setNorthWest("X");
+  function handleButtonClick(index) {
+    const newButtonTexts = [...buttonTexts];
+    newButtonTexts.fill("");
+    newButtonTexts[index] = "X";
+    setButtonTexts(newButtonTexts);
   }
 
   return (
@@ -42,18 +39,87 @@ export default function Main() {
       </div>
 
       <div className="container-image">
-        <button id="northNorth" onClick={handleNorthNorthClick}>
-          {northNorth}
-        </button>
-        <button id="northNorth" onClick={handleNorthSouthClick}>
-          {northSouth}
-        </button>
-        <button id="northNorth" onClick={handleNorthEastClick}>
-          {northEast}
-        </button>
-        <button id="northNorth" onClick={handleNorthWestClick}>
-          {northWest}
-        </button>
+        {/* NORTH */}
+        <div className="northpart">
+          <button id="northNorth" onClick={() => handleButtonClick(0)}>
+            {buttonTexts[0]}
+          </button>
+          <button id="northSouth" onClick={() => handleButtonClick(1)}>
+            {buttonTexts[1]}
+          </button>
+          <button id="northEast" onClick={() => handleButtonClick(2)}>
+            {buttonTexts[2]}
+          </button>
+          <button id="northWest" onClick={() => handleButtonClick(3)}>
+            {buttonTexts[3]}
+          </button>
+        </div>
+
+        {/* SOUTH */}
+        <div className="southpart">
+          <button id="southNorth" onClick={() => handleButtonClick(4)}>
+            {buttonTexts[4]}
+          </button>
+          <button id="southSouth" onClick={() => handleButtonClick(5)}>
+            {buttonTexts[5]}
+          </button>
+          <button id="southEast" onClick={() => handleButtonClick(6)}>
+            {buttonTexts[6]}
+          </button>
+          <button id="southWest" onClick={() => handleButtonClick(7)}>
+            {buttonTexts[7]}
+          </button>
+        </div>
+
+        {/* EAST */}
+        <div className="eastpart">
+          <button id="eastNorth" onClick={() => handleButtonClick(8)}>
+            {buttonTexts[8]}
+          </button>
+          <button id="eastSouth" onClick={() => handleButtonClick(9)}>
+            {buttonTexts[9]}
+          </button>
+          <button id="eastEast" onClick={() => handleButtonClick(10)}>
+            {buttonTexts[10]}
+          </button>
+          <button id="eastWest" onClick={() => handleButtonClick(11)}>
+            {buttonTexts[11]}
+          </button>
+        </div>
+
+        {/* WEST */}
+        <div className="westpart">
+          <button id="westNorth" onClick={() => handleButtonClick(12)}>
+            {buttonTexts[12]}
+          </button>
+          <button id="westSouth" onClick={() => handleButtonClick(13)}>
+            {buttonTexts[13]}
+          </button>
+          <button id="westEast" onClick={() => handleButtonClick(14)}>
+            {buttonTexts[14]}
+          </button>
+          <button id="westWest" onClick={() => handleButtonClick(15)}>
+            {buttonTexts[15]}
+          </button>
+        </div>
+        {/* CENTER */}
+        <div className="centerpart">
+          <button id="centerNorth" onClick={() => handleButtonClick(16)}>
+            {buttonTexts[16]}
+          </button>
+          <button id="centerSouth" onClick={() => handleButtonClick(17)}>
+            {buttonTexts[17]}
+          </button>
+          <button id="centerEast" onClick={() => handleButtonClick(18)}>
+            {buttonTexts[18]}
+          </button>
+          <button id="centerWest" onClick={() => handleButtonClick(19)}>
+            {buttonTexts[19]}
+          </button>
+          <button id="centerCenter" onClick={() => handleButtonClick(20)}>
+            {buttonTexts[20]}
+          </button>
+        </div>
 
         <img src={bgImage} alt="" />
       </div>
